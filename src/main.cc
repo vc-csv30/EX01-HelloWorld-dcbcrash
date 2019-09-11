3665dcb1f67a5c5474cd09a76f0324cde9badd06
dcbcrash <dcbcrash@gmail.com>
2019-09-11 11:36:05 -0700

    hjklkjh

--- src/main.cc
+++ src/main.cc
@@ -12,7 +12,7 @@
 #include <csv30>
 
 int main(int argc, char *argv[]) {
- std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
     return EXIT_SUCCESS;
 }
 // Created by aknight on 8/2/19.
